04cc4fa4def3e966d62682517c3d5e7ef1177cb0
Matsvei Pahotski <matsvei.pahotski@gmail.com>
2022-10-31 01:10:57 +0100

    Add function descriptions in VoIP_Calculator

--- src/VoIP_Calculator/VoIP_Calculator.py
+++ src/VoIP_Calculator/VoIP_Calculator.py
@@ -15,20 +15,35 @@ class VoIP_Calculator(object):
 
     def _voice_payload_size(self) -> float:
         """
-        ( codec_sample_interval[ms] * codec_bit_rate[kbps] ) / 8bit = x Bytes
-
-        :return: VPS in Bytes
+        Count voice payload size that represents the number of bytes
+        that are filled into a packet.
+        The voice payload size must be a multiple of the codec sample size.
+        :return: voice payload size in Bytes
         """
         return (self.n * self.codec_sample_interval * self.codec_bit_rate) / 8
 
     def _total_packet_size(self) -> float:
+        """
+        Count total packet size that consists of bytes of
+        protocol header together with voice payload.
+        :return: packet size in Bytes
+        """
         return self.headers + self._voice_payload_size()
 
     def _calculate_packet_rate(self) -> float:
+        """
+        Packet rate represents the number of packets that need to be
+        transmitted every second in order to deliver the codec bit rate.
+        :return: packet rate in pps
+        """
         packet_rate = (self.codec_bit_rate * 1000) / (self._voice_payload_size() * 8)
         return round(packet_rate, 1)
 
     def _calculate_bandwidth(self) -> float:
+        """
+        Calculate VoIP bandwidth.
+        :return: bandwidth in kbps
+        """
         bandwidth = (self._total_packet_size() * self._packet_rate * 8) / 1000
         return round(bandwidth, 1)
 

a9c89524c79a3857462451042f11a26984d1982d
Matsvei Pahotski <matsvei.pahotski@gmail.com>
2022-12-26 01:49:30 +0100

    Fix tests to suit current implementation

--- test/test_bandwidth_calculations.py
+++ test/test_bandwidth_calculations.py
@@ -6,61 +6,69 @@ from src.VoIP_Calculator.Header import Header
 
 def test_voip_calculator_bandwidth_calculations_for_RTP_protocol() -> None:
     """
-    Test checks how VoIP_Calculator calculates packet
-    rate for several codecs with "RTP" protocol
+    Test checks how VoIP_Calculator calculates bandwidth
+    for several codecs with "RTP" protocol
     and one some sample interval.
     """
+    voip_calculator = VoIP_Calculator()
     headers = Header(["RTP"])
 
-    voip_with_bandwidth = {
-        VoIP_Calculator(headers, Codec("G.711 64kbps", sample_interval=20)):      68.8,
-        VoIP_Calculator(headers, Codec("G.729 8kbps", sample_interval=20)):        12.8,
-        VoIP_Calculator(headers, Codec("G.729 8kbps", sample_interval=1000)):      8.1,
-        VoIP_Calculator(headers, Codec("G.723.1 5.3kbps", sample_interval=30)):  8.5
+    set_of_parameters = {
+        (headers, Codec("G.711 64kbps", voice_payload_size=20)): 68.8,
+        (headers, Codec("G.729 8kbps", voice_payload_size=20)): 12.8,
+        (headers, Codec("G.729 8kbps", voice_payload_size=1000)): 8.1,
+        (headers, Codec("G.723.1 5.3kbps", voice_payload_size=30)): 8.5
     }
 
-    for calculator, expected_bandwidth in voip_with_bandwidth.items():
-        assert (calculator.get_bandwidth() == expected_bandwidth)
+    for parameter, expected_bandwidth in set_of_parameters.items():
+        voip_calculator.calculate(parameter[0], parameter[1])
+        assert (voip_calculator.get_bandwidth() == expected_bandwidth)
 
 
 def test_voip_calculator_bandwidth_calculations_for_UDP_protocol() -> None:
     """
-    Test checks how VoIP_Calculator calculates packet
-    rate for several codecs with additional headers ("RTP+"UDP")
+    Test checks how VoIP_Calculator calculates bandwidth
+    for several codecs with additional headers ("RTP+"UDP")
     and one some sample interval.
     """
+    voip_calculator = VoIP_Calculator()
     headers = Header(["RTP", "UDP"])
 
-    voip_with_bandwidth = {
-        VoIP_Calculator(headers, Codec("G.711 64kbps", sample_interval=20)):        72,
-        VoIP_Calculator(headers, Codec("G.729 8kbps", sample_interval=20)):         16,
-        VoIP_Calculator(headers, Codec("G.729 8kbps", sample_interval=1000)):       8.2,
-        VoIP_Calculator(headers, Codec("G.723.1 5.3kbps", sample_interval=90)):     7.1,
-        VoIP_Calculator(headers, Codec("G.728 12.8kbps", sample_interval=90)):      14.6
+    set_of_parameters = {
+        (headers, Codec("G.711 64kbps", voice_payload_size=20)): 72,
+        (headers, Codec("G.729 8kbps", voice_payload_size=20)): 16,
+        (headers, Codec("G.729 8kbps", voice_payload_size=1000)): 8.2,
+        (headers, Codec("G.723.1 5.3kbps", voice_payload_size=90)): 7.1,
+        (headers, Codec("G.728 12.8kbps", voice_payload_size=90)): 14.6
     }
 
-    for calculator, expected_bandwidth in voip_with_bandwidth.items():
-        assert (calculator.get_bandwidth() == expected_bandwidth)
+    for parameter, expected_bandwidth in set_of_parameters.items():
+        voip_calculator.calculate(parameter[0], parameter[1])
+        assert (voip_calculator.get_bandwidth() == expected_bandwidth)
 
 
-def test_voip_calculator_bandwidth_calculations_for_sets_of_protocols() -> None:
+def test_voip_calculator_for_sets_of_protocols() -> None:
     """
-    Test checks how VoIP_Calculator calculates packet
+    Test checks how VoIP_Calculator calculates bandwidth and packet
     rate for several codecs, protocol set and frames.
     """
 
-    voip_with_bandwidth = {
-        VoIP_Calculator(Header(["RTP", "UDP", "IPv4", "ethernet 802.3"]),
-                        Codec("G.711 64kbps", frames=160)):                             95.2,
-        VoIP_Calculator(Header(["RTP", "UDP", "IPv6"]),
-                        Codec("G.729 8kbps", sample_interval=20), channels=2):          64,
-        VoIP_Calculator(Header(["RTP", "UDP", "IPv4", "ethernet 802.3"]),
-                        Codec("G.729 8kbps", sample_interval=20)):                      39.2,
-        VoIP_Calculator(Header(["RTP", "UDP", "IPv4", "ethernet 802.3"]),
-                        Codec("G.723.1 5.3kbps", frames=3)):                            12.2,
-        VoIP_Calculator(Header(["RTP", "UDP", "IPv6", "ethernet 802.3"]),
-                        Codec("G.723.1 5.3kbps", frames=3), channels=3):                42,
+    voip_calculator = VoIP_Calculator()
+
+    set_of_parameters = {
+        (Header(["RTP", "UDP", "IPv4", "ethernet 802.3"]),
+         Codec("G.711 64kbps", frames=2), 1):               (95.2, 50),
+        (Header(["RTP", "UDP", "IPv6"]),
+         Codec("G.729 8kbps", voice_payload_size=20), 2):   (64, 100),
+        (Header(["RTP", "UDP", "IPv4", "ethernet 802.3"]),
+         Codec("G.729 8kbps", voice_payload_size=20), 1):   (39.2, 50),
+        (Header(["RTP", "UDP", "IPv4", "ethernet 802.3"]),
+         Codec("G.723.1 5.3kbps", frames=3), 1):            (12.2, 11.1),
+        (Header(["RTP", "UDP", "IPv6", "ethernet 802.3"]),
+         Codec("G.723.1 5.3kbps", frames=3), 3):            (42, 33.3)
     }
 
-    for calculator, expected_bandwidth in voip_with_bandwidth.items():
-        assert (calculator.get_bandwidth() == expected_bandwidth)
+    for parameter, expected_out in set_of_parameters.items():
+        voip_calculator.calculate(parameter[0], parameter[1], parameter[2])
+        assert (voip_calculator.get_bandwidth() == expected_out[0])
+        assert (voip_calculator.get_pps() == expected_out[1])
--- test/test_codec_calculations.py
+++ test/test_codec_calculations.py
@@ -13,10 +13,10 @@ def test_codec_frames_per_packet_calculations() -> None:
     based on bit rate and sample interval.
     """
     codec_sets_with_out = {
-        Codec("G.711 64kbps", sample_interval=0.125):     1,
-        Codec("G.711 64kbps", sample_interval=10.0):      80,
-        Codec("G.728 12.8kbps", sample_interval=10.0):    16,
-        Codec("G.729 8kbps", sample_interval=20.0):       2
+        Codec("G.711 64kbps", voice_payload_size=10):        1,
+        Codec("G.711 64kbps", voice_payload_size=40):        4,
+        Codec("G.728 12.8kbps", voice_payload_size=10.0):    2,
+        Codec("G.729 8kbps", voice_payload_size=20.0):       2
     }
 
     for codec, expected_out in codec_sets_with_out.items():

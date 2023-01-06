af6c913fc14e4e920c66f77ba5d4ed3809abfc8c
Matsvei Pahotski <matsvei.pahotski@gmail.com>
2023-01-06 22:01:19 +0100

    Add _name and _dimensions initialization to base class

--- src/Ui/Widgets/WidgetTemplate.py
+++ src/Ui/Widgets/WidgetTemplate.py
@@ -6,6 +6,8 @@ class WidgetTemplate(Ui_Font, object):
     def __init__(self, widget: QtWidgets):
         super().__init__()
         self._widget = widget
+        self._name = None
+        self._dimensions = None
 
     def setup(self, name: str, dimensions: QtCore.QSize):
         """Perform ui_template object setup"""
